4e5d9e6ea47ee226bb08c7d3e7de95a0bca04a68
Gustavo Rodriguez <garodrifornes79@gmail.com>
2025-08-19 00:10:40 -0300

    Agrego lección de Método de Ordenamiento y Multiplicación de lista

---
+++ Python/Clase2/Ordenamientos.py
@@ -0,0 +1,16 @@
+lista = [1, 7, 9, 2, 4, 6, 3, 5, 8]
+
+# Multiplicar elementos de una lista 
+lista2 = (lista) * 2
+print(lista2)
+
+# Métodos de ordenamiento Ascendente
+lista.sort()
+print(lista)
+
+# Método de ordenamiento Descendente
+lista.sort(reverse=True)
+print(lista)
+
+
+
