e468799a7c2502f01dda9f38f4b9314c0ac7edc1
Nahuel Spikerman <nahuelspikerman@gmail.com>
2025-08-21 15:15:21 +0000

    Clase 2 - JavaScript - ArreglosParte3

--- JavaScript/Clase2/Arreglos.js
+++ JavaScript/Clase2/Arreglos.js
@@ -1,2 +1,19 @@
 //let autos= new Array("Ferrari", "Renault", "BMW"); //Esta es la sintaxis vieja
 const autos = ["Ferrari", "Renault", "BMW"]
+
+// Modificamos los elementos del arreglo
+
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+// Agregamos nuevos valores al arreglo
+autos.push('Audi'); // Agregamos el elementos al final del arreglo
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
+console.log(auto);
+
+// Tercera forma de agregar elementos teniendo cuidado
+autos[6] = 'Renault'
+console.log(autos);
