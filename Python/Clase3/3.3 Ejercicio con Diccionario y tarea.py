1b55e3a8d9b84ba21b3cfa33075d65767e37e806
Agustin <santarenaagustin69@gmail.com>
2025-08-25 19:59:08 -0300

    3.3 Ejercicio con Diccionario y tarea

---
+++ Python/Clase3/3.3 Ejercicio con Diccionario y tarea.py
@@ -0,0 +1,12 @@
+#La tarea consiste en ingresar elementos al diccionario llamado seleccionArgentina,lo elementos a ingresar deben ser como mínimo 4, 
+# estos elementos son los jugadores con su número de camiseta, nombre, apellido, edad, altura, precio y posición de juego, 
+# por supuesto ver el video anterior.
+
+seleccionArgentina = {
+    10: {"Nombre":"Lionel Messi", "Edad":36, "Altura":1.70, "Precio":18000000, "Posición":"Delantero"},
+    9: {"Nombre":"Julian Alvarez", "Edad":23, "Altura":1.70, "Precio":100000000, "Posición":"Delantero"},
+    23: {"Nombre":"Emiliano Martinez", "Edad":31, "Altura":1.95, "Precio":20000000, "Posición":"Arquero"},
+    24: {"Nombre":"Enzo Fernandez", "Edad":24, "Altura":1.78, "Precio":50000000, "Posición":"Mediocampista"}
+}
+
+print(seleccionArgentina[24])
