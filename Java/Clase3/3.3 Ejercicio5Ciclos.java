f73ddb15ef8be67543c291bb65395a87161fba8b
gabriel santarena <gabrielsantarena69@gmail.com>
2025-08-27 19:01:58 -0300

    Clase 3 - Java - Ejercicio5Ciclo

---
+++ Java/Clase3/3.3 Ejercicio5Ciclos.java
@@ -0,0 +1,33 @@
+package Clase3;
+
+import java.util.Scanner;
+
+public class Ejercicio5Ciclo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int numero, numaleatorio, contador = 0;
+
+        numaleatorio = (int)(Math.random() * 100); // Esto genera un numero aleatorio de 0 a 100
+
+        do{
+        System.out.println("Digite un numoer : ");
+        numero = Integer.parseInt(entrada.nextLine());
+
+        if (numero < numaleatorio){
+            System.out.println("Digite un numero mayor");
+        }
+        else if (numero > numaleatorio){
+            System.out.println("Ingrese un numero menor");
+        }
+        else{
+            System.out.println("\t!!FELICIDADES¡¡ Hacertaste el numero");
+        }
+        contador++;
+
+    }while(numero != numaleatorio);
+
+    System.out.println("\tAdivinaste el numero en " + contador + " intentos");
+    
+    }
+}
