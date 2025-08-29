c666df6aec50d4bb5287819a509bf4f0db509de1
Agustin <santarenaagustin69@gmail.com>
2025-08-29 18:46:58 -0300

    Update Ejercicio5Ciclo.java

--- Java/Clase3/Ejercicio5Ciclo.java
+++ Java/Clase3/Ejercicio5Ciclo.java
@@ -2,13 +2,15 @@ package Clase3;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio5Ciclo {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        //Scanner entrada = new Scanner(System.in);
 
         int numero, numaleatorio, contador = 0;
 
-        numaleatorio = (int)(Math.random() * 100); // Esto genera un numero aleatorio de 0 a 100
+        /*numaleatorio = (int)(Math.random() * 100); // Esto genera un numero aleatorio de 0 a 100
 
         do{
         System.out.println("Digite un numoer : ");
@@ -25,9 +27,30 @@ public class Ejercicio5Ciclo {
         }
         contador++;
 
-    }while(numero != numaleatorio);
-    entrada.close();
-    System.out.println("\tAdivinaste el numero en " + contador + " intentos");
+        }while(numero != numaleatorio);
+        entrada.close();
+        System.out.println("\tAdivinaste el numero en " + contador + " intentos");*/
     
+    
+        numaleatorio = (int)(Math.random() * 100);
+
+        do{
+            String num = JOptionPane.showInputDialog("Digite un numero: ");
+            numero = Integer.parseInt(num);
+            if(numero < numaleatorio){
+                JOptionPane.showMessageDialog(null, "Digite un numero mayor");
+                numero = Integer.parseInt(num);
+            }
+            else if (numero > numaleatorio){
+                JOptionPane.showMessageDialog(null, "Ingrese un numero menor");
+                numero = Integer.parseInt(num);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "\t!!FELICIDADES¡¡ Hacertaste el numero");
+            }
+            contador++;
+        }
+        while(numero != numaleatorio);
+        JOptionPane.showMessageDialog(null, "\tAdivinaste el numero en " + contador + " intentos");
     }
 }
