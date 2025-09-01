8dc347013b917b06580b7ff2343e9e2788635ebc
Damián Adolfo Ponce de León <poncedeleondamianadolfo@gmail.com>
2025-09-01 01:41:51 -0300

    Create 3.5Colas.py

---
+++ Python/Clase3/3.5Colas.py
@@ -0,0 +1,25 @@
+# Colas con Listas
+# Estructura de Datos tipo fifo(First input / First output )
+cola=['Ariel', 'Osvaldo', 'Liliana', 'Pilar']
+
+# Agregamos elementos al final de la Cola 
+cola.append('Natalia')
+cola.append('Jose')
+print(cola)
+
+#Sacamos Elemento de la Cola
+seRetira = cola(0).pop(0)
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola(0).pop(0)
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola(0).pop(0)
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola(0).pop(0)
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
