803c56668bff626c51dc222e2f784bb346f78ceb
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-03 23:37:36 +0000

    Agregué la clase 4 de Java

---
+++ Java/Clase4/Clases/Persona.java
@@ -0,0 +1,17 @@
+
+package Clases;
+
+
+public class Persona {
+    //Atributos de la clases (Caracteristicas)
+    String nombre;
+    String apellido;
+    
+    
+    //Métodos de la clase (Acciones)
+    public void obternetInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+        
+    }
+}
