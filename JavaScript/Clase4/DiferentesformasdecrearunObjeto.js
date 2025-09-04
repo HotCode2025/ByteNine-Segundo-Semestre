71debc0a13a0108c4b33f281e1780cbd3cd5a117
AgusSantarena <santarenaagustin69@gmail.com>
2025-09-03 22:39:57 -0300

    Delete JavaScript/Clase4

--- JavaScript/Clase4/DiferentesformasdecrearunObjeto.js
+++
@@ -1,8 +0,0 @@
-let persona2 = new Object();
-persona2.nombre = "Carlos";
-persona2.direccion = "Calle Falsa 123";
-persona2.telefono = "55443322";
-
-console.log(persona2.telefono);
-console.log("creamos un nuevo objeto: ");
-console.log(persona["apellido"]);
