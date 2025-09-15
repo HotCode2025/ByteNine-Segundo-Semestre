a6d5d6c8a41c67db3945554ff8621f040e9cf8de
Nahuel Spikerman <nahuelspikerman@gmail.com>
2025-09-15 20:45:54 -0300

    8.2 Atributos en métodos y creación de un objeto

--- Python/Leccion6/Persona.py
+++ Python/Leccion6/Persona.py
@@ -1,4 +1,11 @@
 class Persona: #Creamos una clase
-    pass # No se procesa nada mas (No tiene contenido)
 
-print(type(Persona))
+    def __init__(self):  # Se lo llama método Init Dunder
+        self.nombre = 'Juan'
+        self.apellido = 'Zalazar'
+        self.edad = 22
+
+persona1 = Persona()
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
