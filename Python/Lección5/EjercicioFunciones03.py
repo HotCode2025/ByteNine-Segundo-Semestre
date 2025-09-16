4e69bc8a0cc76a6d1c07e85d1c935dfaec2426c3
Nahuel Spikerman <nahuelspikerman@gmail.com>
2025-09-15 22:01:37 -0300

    Python - Clase 5 - EjercicioFunciones03

--- Python/Lección5/EjercicioFunciones03.py
+++ Python/Lección5/EjercicioFunciones03.py
@@ -2,15 +2,16 @@
 #Punto: 7.5 Ejercicio 3
 # Ejercicio 3: Funcion recursiva
 #Imprimir numeros de 5 a 1 de manera descendente usando funciones recursivas
-#Puede ser cualquier valor positivo, por ejemplo, si pasamos el
-#valor de 5, debe imprimir:
-#5
-#4
-#3
-#2
-#1
-#En el caso de ser el numero 3 debe imprimir:
-#3
-#2
-#1
-#Si se ingresn numeros negativos no se imprime nada
+def imprimir_descendente(n):
+    #Si n <= 0, no imprime nada
+    if n <= 0:
+        return
+    #Imprimir el número actual
+    print(n)
+    #Llamada recursiva con n - 1
+    imprimir_descendente(n - 1)
+
+
+#Programa principal
+numero = int(input("Ingrese un número positivo: "))
+imprimir_descendente(numero)
