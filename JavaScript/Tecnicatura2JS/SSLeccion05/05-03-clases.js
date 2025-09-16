0530fca375a1ab4b07162dd1500d18995ce68d7f
Agustin <santarenaagustin69@gmail.com>
2025-09-15 21:11:21 -0300

    Clase N°6 - Sintaxis de Clases en JavaScript - JavaScript

---
+++ JavaScript/Tecnicatura2JS/SSLeccion05/05-03-clases.js
@@ -0,0 +1,12 @@
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+let Persona1 = new Persona("Martin", "Perez");
+console.log(Persona1);
+
+let Persona2 = new Persona("Carlos", "Lara");
+console.log(Persona2);
