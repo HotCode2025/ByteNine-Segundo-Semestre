0aee58bcb59b4075b536ef7dc50a8634a7fdef0f
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-16 15:25:01 -0300

    Agregué mi parte

--- Python/Leccion6/Persona.py
+++ Python/Leccion6/Persona.py
@@ -6,6 +6,9 @@ class Persona: #Creamos una clase
         self.edad = edad
 
 persona1 = Persona('Ariel', 'Betancud', 40) # Necesitamos enviar argumentos
+# print(persona1.nombre) # Tarea: Hacer el print igual que con el objeto 2
+# print(persona1.apellido)
+# print(persona1.edad)
 print(persona1.nombre)
 print(persona1.apellido)
 print(persona1.edad)
@@ -13,3 +16,6 @@ print(persona1.edad)
 persona2 = Persona('Osvaldo', 'Giordanini', 45)
 
 print(f"El objeto 2 de la clase Peronsa es: {persona2.nombre} {persona2.apellido} {persona2.edad}")
+
+# Los atributos son: caracteriscas
+# Los métodos son: el comportamiento que van a tener los objetos (acciones)
