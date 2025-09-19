026152f22e8096868ea3ef1b00dee4c749f2fd9f
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-18 22:19:17 -0300

    Update EjercicioFunciones01.py

--- Python/Lección5/EjercicioFunciones01.py
+++ Python/Lección5/EjercicioFunciones01.py
@@ -3,7 +3,7 @@
 #Ejercicio 01: Crear una funcion para sumar los valores recibidos de tipo
 #numerico, utilizando argumentos variables *args como parametro de la
 #Funcion y agregar como resultado la suma de todos los valores pasados
-#como argumentos
+#como argumentos 
 
 def sumar(*args):
     return sum(args)
