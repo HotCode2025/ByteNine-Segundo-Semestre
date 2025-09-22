d1c38d761d7058b9003071f73299c2b881b52152
santy171004 <santinmartin773@gmail.com>
2025-09-22 19:43:57 -0300

    Clase 5 - Ejercicio2 - Python

--- Python/Lección5/EjercicioFunciones02.py
+++ Python/Lección5/EjercicioFunciones02.py
@@ -1,3 +1,12 @@
 #CLASE 5-Tema: Funciones Recursivas Parte 3  -> Tarea(08-09)
 #Punto: 7.1 Ejercicio 2 con funciones y argumentos variables
 #resolviendo dudas
+
+def multiplicar(*args):
+    resultado = 1
+    for numero in args:
+        resultado *= numero
+    return resultado
+
+print(multiplicar(2, 3))
+print(multiplicar(1, 2, 3, 7))
