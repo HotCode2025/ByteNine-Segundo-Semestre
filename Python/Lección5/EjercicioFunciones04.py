6d0616ba7327e3d57ddfb7d53ba4ef7f22156eb5
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2025-09-22 19:35:46 -0300

    Clase 5 Ejercicio4 Python

--- Python/Lección5/EjercicioFunciones04.py
+++ Python/Lección5/EjercicioFunciones04.py
@@ -7,5 +7,20 @@
 #Proporcione el pago sin impuestos: 1000
 #Proporcione el monto del impuesto: 21%
 #Pago con impuesto: xxxxx
+# Prueba de configuracion.
 
-#Prueba de configuracion.
+# Defino el porcentaje del IVA como una constante (21%)
+IVA = 21
+# Funcion que calcula el pago total incluyendo el impuesto IVA
+def calcular_pago_con_impuesto(pago_sin_impuesto):
+    # Formula: pago_sin_impuesto + (pago_sin_impuesto * porcentaje_IVA)
+    # Ejemplo: 100 + (100 * 0.21) = 121
+    pago_total = pago_sin_impuesto + pago_sin_impuesto * (IVA /100) # Formmula
+    return pago_total
+# Solicitamos al usuario que ingrese el monto sin impuesto
+pago = float(input("Digite su pago sin impuesto: "))
+# Calculamos el total llamando a la funcion
+total = calcular_pago_con_impuesto(pago)
+# Mostramos el resultado
+print(f"El pago con IVA({IVA}%): ${total:.2f}")
+
