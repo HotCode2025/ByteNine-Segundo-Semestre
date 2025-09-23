c85e5b43616e4a88a4fdc9a0d0f4e4785d3180dc
Valeria  Lardin <valelar81@gmail.com>
2025-09-23 10:11:01 -0300

    Arreglo problema de identacion

--- Python/Leccion6/Cubo.py
+++ Python/Leccion6/Cubo.py
@@ -1,37 +1,36 @@
 class Cubo:
-    class Cubo:
+    """
+    Clase 7: Tema Poo Parte 2-Tarea(22-09)
+    Crear la clase Cubo con los atributos, ancho, alto y profundidad,
+    con un metpdo calcular_volumen que tendrá la formula:
+    volumen = ancho * altura * profundidad
+    que el usuario ingrese los valores
+    """
+
+    def __init__(self, ancho, alto, profundidad):
         """
-        Clase 7: Tema Poo Parte 2-Tarea(22-09)
-        Crear la clase Cubo con los atributos, ancho, alto y profundidad,
-        con un metpdo calcular_volumen que tendrá la formula:
-        volumen = ancho * altura * profundidad
-        que el usuario ingrese los valores
+        Metodo constructor(inicializamos los atributos del cubo
         """
+        self.ancho = ancho  # Lo digita el usuario
+        self.alto = alto  # Asignacion, lo digita el usuario
+        self.profundidad = profundidad  # la digita el usuario
 
-        def __init__(self, ancho, alto, profundidad):
-            """
-            Metodo constructor(inicializamos los atributos del cubo
-            """
-            self.ancho = ancho  # Lo digita el usuario
-            self.alto = alto  # Asignacion, lo digita el usuario
-            self.profundidad = profundidad  # la digita el usuario
+    def calcular_volumen(self):
+        """
+        Metodo para calcular el volumen
+        """
+        return self.ancho * self.alto * self.profundidad
 
-        def calcular_volumen(self):
-            """
-            Metodo para calcular el volumen
-            """
-            return self.ancho * self.alto * self.profundidad
+# Pedir al usuario que digito los valores del cubo
+ancho_digitado = float(input("Digite el ancho del cubo: "))
+alto_digitado = float(input(" Digite el alto del cubo: "))
+profundidad_digitada = float(input("Digite la profundidad del cubo: "))
 
-    # Pedir al usuario que digito los valores del cubo
-    ancho_digitado = float(input("Digite el ancho del cubo: "))
-    alto_digitado = float(input(" Digite el alto del cubo: "))
-    profundidad_digitada = float(input("Digite la profundidad del cubo: "))
+# Instancia del objeto de la clase CUbo
+el_cubo = Cubo(ancho_digitado, alto_digitado, profundidad_digitada)
 
-    # Instancia del objeto de la clase CUbo
-    el_cubo = Cubo(ancho_digitado, alto_digitado, profundidad_digitada)
+# Llamamos el metodo calcular_volumen
+volumen = el_cubo.calcular_volumen()
 
-    # Llamamos el metodo calcular_volumen
-    volumen = el_cubo.calcular_volumen()
-
-    # Mostrar resultados
-    print(f"El volumen del cubo es: {volumen}")
+# Mostrar resultados
+print(f"El volumen del cubo es: {volumen}")
