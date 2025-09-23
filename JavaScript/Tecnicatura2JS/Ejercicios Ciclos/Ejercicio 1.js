ea1e1dd5307deb7548fb0eca60d1503cf580275d
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2025-09-23 05:51:22 -0300

    Ejercicio1 - Ciclo - JavaScript

---
+++ JavaScript/Tecnicatura2JS/Ejercicios Ciclos/Ejercicio 1.js
@@ -0,0 +1,9 @@
+//Ejercicio 1 Pseint --> Javascript
+//Imprimir en pantalla los numeros del 1 al 10.(en columna)
+
+let numero = 1;
+
+while (numero <= 10){
+    console.log(numero);
+    numero++;
+}
