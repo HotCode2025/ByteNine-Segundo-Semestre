e7dd6d710a84a2cfa6fc5aa783c6bd924b78a14f
santy171004 <santinmartin773@gmail.com>
2025-09-23 13:05:54 -0300

    Ejercicio 03 - Ciclo - JavaScript

---
+++ JavaScript/Tecnicatura2JS/Ejercicios Ciclos/Ejercicio 03.js
@@ -0,0 +1,9 @@
+//Pseint -> JS
+//imprimir en pantalla los numeros del 10 al 1.(en fila y con saltos de linea)
+// con un while
+
+let numero = 10;
+while (numero >= 1) {
+    console.log(numero);
+    numero--;
+}
