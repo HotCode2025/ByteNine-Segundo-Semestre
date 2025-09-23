243f558953a6bea3b3ea6b1017021f7b42625b21
santy171004 <santinmartin773@gmail.com>
2025-09-22 21:43:34 -0300

    Clase 7 - Crear la clase Aritmética_sumar - Python

--- Python/Leccion6/Aritmetica.py
+++ Python/Leccion6/Aritmetica.py
@@ -1,1 +1,19 @@
 #Clase 7 POO Parte 2 Tarea (22/09)
+class Aritmetica:
+    """
+    este tipo de comentario se llama docstring
+    y sirve para documentar la clase
+    vamos a hacer algunas opreraciones de suma, resta, multiplicacion y division
+    """
+
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+
+    # metodo de suma
+    def sumar(self):
+        return self.operandoA + self.operandoB
+
+
+Aritmetica1 = Aritmetica(5, 3)  # le pasamos los argumentos para los operandos
+print(Aritmetica1.sumar())
