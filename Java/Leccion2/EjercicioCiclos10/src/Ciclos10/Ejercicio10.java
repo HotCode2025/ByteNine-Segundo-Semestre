d0b8d402a9c4abb528631961d6741eafbf375c33
Valeria  Lardin <valelar81@gmail.com>
2025-09-24 19:01:18 -0300

    Creacion de los proyectos de tareas clase 6

---
+++ Java/Leccion2/EjercicioCiclos10/src/Ciclos10/Ejercicio10.java
@@ -0,0 +1,10 @@
+/*CLASE 6:A. Programacion Orientada a objetos parte 4-Tarea
+Punto: 6.8 Ejercicio Ciclos 10: Con Clase Scanner y JOption*/
+/*Ejercicio 10: Pedir 10 numeros y escribir la suma total
+Hacerlo con la clase JOptionPane*/
+package Ciclos10;
+
+
+public class Ejercicio10 {
+    
+}
---
+++ Java/Leccion2/EjercicioCiclos12/src/Ciclos12/Ciclos12.java
@@ -0,0 +1,10 @@
+/*CLASE 6:B. Programacion Orientada a objetos parte 5-Tarea
+Punto: 7.3 Ejercicio Ciclos 12: Con Clase Scanner y JOption*/
+/*Ejercicio 12: Pedir un numero y calcular su factorial
+Hacerlo con la clase Scanner*/
+package Ciclos12;
+
+
+public class Ciclos12 {
+    
+}
