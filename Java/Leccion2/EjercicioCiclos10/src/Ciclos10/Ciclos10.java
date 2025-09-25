c1b18bcc65274ab5767de0fe162c15d90f30298d
Nahuel Spikerman <nahuelspikerman@gmail.com>
2025-09-24 21:53:22 -0300

    Clase 6 - 6.8 Ejercicio Ciclos 10 - Java

--- Java/Leccion2/EjercicioCiclos10/src/Ciclos10/Ciclos10.java
+++ Java/Leccion2/EjercicioCiclos10/src/Ciclos10/Ciclos10.java
@@ -2,9 +2,19 @@
 Punto: 6.8 Ejercicio Ciclos 10: Con Clase Scanner y JOption*/
 /*Ejercicio 10: Pedir 10 numeros y escribir la suma total
 Hacerlo con la clase Scanner*/
-package Ciclos10;
+package ciclos10;
 
+import javax.swing.JOptionPane;
 
 public class Ciclos10 {
-    
+    public static void main(String[] args) {
+        // Creamos un objeto de la clase Ejercicio10
+        Ejercicio10 ejercicio = new Ejercicio10();
+
+	// Se llama al método pedirNumeros que hace la suma
+        int suma = ejercicio.pedirNumeros();
+        
+        // Mostramos el resultado con JOptionPane
+        JOptionPane.showMessageDialog(null, "La suma total es: " + suma);
+    }
 }
--- Java/Leccion2/EjercicioCiclos10/src/Ciclos10/Ejercicio10.java
+++ Java/Leccion2/EjercicioCiclos10/src/Ciclos10/Ejercicio10.java
@@ -2,9 +2,22 @@
 Punto: 6.8 Ejercicio Ciclos 10: Con Clase Scanner y JOption*/
 /*Ejercicio 10: Pedir 10 numeros y escribir la suma total
 Hacerlo con la clase JOptionPane*/
-package Ciclos10;
+package ciclos10;
 
+import java.util.Scanner;
 
 public class Ejercicio10 {
     
+    // Método que pide 10 números con Scanner y devuelve la suma	
+    public int pedirNumeros() {
+        Scanner entrada = new Scanner(System.in);
+        int suma = 0;
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Digite el número " + i + ": ");
+            int numero = entrada.nextInt(); // lee el número del usuario
+            suma += numero; // acumula la suma
+        }
+        return suma; // devuelve la suma total
+    }
 }
