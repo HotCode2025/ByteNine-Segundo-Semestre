28e8cb61ca06da1dd8ddd0fae2b48ee3868ec84e
Valeria  Lardin <valelar81@gmail.com>
2025-09-25 23:57:55 -0300

    Clase 6A-Punto 6.1 Sobrecarga de metodos

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -10,35 +10,47 @@ public class Aritmetica {
     int a;//Valor asignado por default 0
     int b;
 // ..................------------------------------
+//Clase 6A:Tema:Programacion orientada a objetos Parte 4
+//Punto 6.1 Sobrecarga de metodos
+//El constructor es un metodo especial
+    public Aritmetica() { //Constructor 1(vacio)
+        System.out.println("Se esta ejecuntando el constructor numero 1");
+    }
+
+    //estamos viendo lo que se llama sobrecarga de constructores
+    public Aritmetica(int a, int b) { //Constructor 2
+        this.a = a;
+        this.b = b;
+        System.out.println("Se esta ejecutando el constructor numero 2");
+    }
+
     //Metodo (definirlo)vacio
     public void sumarNumeros() {
         int resultado = a + b;
-        System.out.println("resultado = " + resultado);
+        System.out.println("resultado = " +resultado);
     }
 
     /*Clase 5; Tema Clases y Objetos Parte 3 Tarea
-Punto: 5.2 Clase Aritmética: Creamos un método, recorremos con Debbug*/
+     Punto: 5.2 Clase Aritmética: Creamos un método, recorremos con Debbug*/
     //Creamos otro metodo(retorna el valor de una expresion
     public int sumarConRetorno() {
         //int resultado = a + b;
-        //return resultado;
-        return this.a + this.b;
+        return a + b;
+
     }
 
     /*Punto: 5.3 Paso de argumentos a un método
     Creamos otro metodo*/
-   /* public int sumarConArgumentos(int arg1, int arg2) {
+ /* public int sumarConArgumentos(int arg1, int arg2) {
         
         a = arg1;
         b = arg2;
         //return a + b;
         //Punto: 5.4 Un método llamando a otro método dentro de la misma classe.
         return sumarConRetorno();*/
-    
-    
-      public int sumarConArgumentos(int a, int b) {
+    public int sumarConArgumentos(int a, int b){
         //Punto: 5.5 Operador this(uso opcional, se crea automaticamente)
-        this.a = b;//El argumento a se asigna al atributo this.a
+        this.a = a;//El argumento a se asigna al atributo this.a
         this.b = b;
         //return a + b;
         //Punto: 5.4 Un método llamando a otro método dentro de la misma classe.
--- Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -1,9 +1,7 @@
-
 //Viene de Clase 4
 /*Clase 5; Tema Clases y Objetos Parte 3 Tarea
 Punto: 5.1 Clase Aritmética: Creamos un objeto*/
 package Operaciones;
-
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
@@ -12,16 +10,26 @@ public class PruebaAritmetica {
         aritmetica1.a = 3;//llamamos a traves del objeto y colocamos valor
         aritmetica1.b = 7;
         aritmetica1.sumarNumeros();
-        
+
         /*Clase 5; Tema Clases y Objetos Parte 3 Tarea
         Punto: 5.2 Clase Aritmética: Creamos un método, recorremos con Debbug*/
         int resultado = aritmetica1.sumarConRetorno();
-        System.out.println("resultado = " + resultado);
-        
+        System.out.println("resultado = " +resultado);
+
         //Punto:5.3 Paso de argumentos a un método
         resultado = aritmetica1.sumarConArgumentos(12, 26);
-        System.out.println("Resultado usando argumentos = "+resultado);
-   
+        System.out.println("Resultado usando argumentos = " +resultado);
+
+        //Clase 6A:Tema:Programacion orientada a objetos Parte 4
+//Punto 6.1 Sobrecarga de metodos
+        System.out.println("aritmetica1  a:  "+aritmetica1.a);
+        System.out.println("aritmetica1  b:  "+aritmetica1.b);
+        
+        Aritmetica arimetica2 = new Aritmetica(5,8);
+        System.out.println("arimetica2 = " + arimetica2.a);
+        System.out.println("arimetica2 = " + arimetica2.b);
+        
+        
     }
 
 }
