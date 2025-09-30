a1c4eabc4ab064d790968b80dc128c060e3183ee
AgusSantarena <santarenaagustin69@gmail.com>
2025-09-29 21:47:12 -0300

    Clase 8 - 10.2 Atributos read-only (solo lectura) - Python

--- Python/Leccion6/Persona2.py
+++ Python/Leccion6/Persona2.py
@@ -33,11 +33,10 @@ class Persona2:
         print("Estamos utilizando el metodo Getter")
         return self._edad
 
-    @edad.setter
-    def edad(self, edad):
-        print("Estamos utilizando el metodo Setter")
-        self._edad= edad
-
+    #@edad.setter
+    #def edad(self, edad):
+        #print("Estamos utilizando el metodo Setter")
+        #self._edad= edad
 
 
 persona1 = Persona2("Ariel", "Bentancud", "41")
@@ -49,3 +48,6 @@ print(persona1.edad)
 persona1.nombre = "Juan Pedro"  #Llamamos al metodo Setter
 print(persona1.nombre) #Nuevamente con el metodo Getter
 print(persona1.mostrar_detalles()) #Llamamos al metodo mostrar_detalles
+
+# Atributo read-only (solo lectura) seria la edad porque no tiene el metodo set
+print(persona1.edad)
