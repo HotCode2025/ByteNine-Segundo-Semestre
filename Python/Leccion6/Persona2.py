3591af15c83d3d4e470cb7824a8f950c75648121
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2025-09-29 23:06:54 -0300

    Clase 8 - 10.5 Comprobación del módulo principal en ejecución

--- Python/Leccion6/Persona2.py
+++ Python/Leccion6/Persona2.py
@@ -33,21 +33,23 @@ class Persona2:
         print("Estamos utilizando el metodo Getter")
         return self._edad
 
-    #@edad.setter
-    #def edad(self, edad):
-        #print("Estamos utilizando el metodo Setter")
-        #self._edad= edad
+    @edad.setter
+    def edad(self, edad):
+        print("Estamos utilizando el metodo Setter")
+        self._edad= edad
 
+if __name__ == '__main__':
+    persona1 = Persona2("Ariel", "Bentancud", "41")
+    #Llamamos al metodo Getter
+    print(persona1.nombre)
+    print(persona1.apellido)
+    print(persona1.edad)
 
-persona1 = Persona2("Ariel", "Bentancud", "41")
-#Llamamos al metodo Getter
-print(persona1.nombre)
-print(persona1.apellido)
-print(persona1.edad)
+    persona1.nombre = "Juan Pedro"  #Llamamos al metodo Setter
+    print(persona1.nombre) #Nuevamente con el metodo Getter
+    print(persona1.mostrar_detalles()) #Llamamos al metodo mostrar_detalles
 
-persona1.nombre = "Juan Pedro"  #Llamamos al metodo Setter
-print(persona1.nombre) #Nuevamente con el metodo Getter
-print(persona1.mostrar_detalles()) #Llamamos al metodo mostrar_detalles
+    # Atributo read-only (solo lectura) seria la edad porque no tiene el metodo set
+    print(persona1.edad)
 
-# Atributo read-only (solo lectura) seria la edad porque no tiene el metodo set
-print(persona1.edad)
+    print(__name__)
--- Python/Leccion6/PruebaPersona2.py
+++ Python/Leccion6/PruebaPersona2.py
@@ -1,3 +1,7 @@
 from Persona2 import Persona2
-persona5 = Persona2('Lionel', 'Messi', 35)
-persona5.mostrar_detalles()
+
+if __name__ == '__main__':
+    persona5 = Persona2('Lionel', 'Messi', 35)
+    persona5.mostrar_detalles()
+
+    print(__name__)
