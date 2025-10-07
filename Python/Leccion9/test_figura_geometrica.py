bb874be16b25c3748704af26c349b45ce4e45dea
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2025-10-07 07:50:02 -0300

    Clase 9 - 12.6 Método MRO: Method Resolution Order

--- Python/Leccion9/test_figura_geometrica.py
+++ Python/Leccion9/test_figura_geometrica.py
@@ -1,6 +1,16 @@
+# Clase 9 POO Parte 5 Herencia Múltiple Tarea
+# 12.4 Creamos la clase para testear nuestro código
+
 from Cuadrado import Cuadrado
 
 cuadrado1 =Cuadrado(5, 'Azul')
 print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(f'Calculo del área del cuadrado: {cuadrado1.calcular_area()}')
+
+# 12.5 Explicación paso a paso con Debug
+
+# 12.6 Método MRO: Method Resolution Order
+
+print(Cuadrado.mro())
+
