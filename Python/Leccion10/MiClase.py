e00b21adbcb1cfdebb72993273eb4d7a7e55ba1f
Nahuel Spikerman <nahuelspikerman@gmail.com>
2025-10-14 13:03:03 -0300

    Clase 10 - 14.4 Contexto estático y Dinámico - Python

--- Python/Leccion10/MiClase.py
+++ Python/Leccion10/MiClase.py
@@ -17,6 +17,12 @@ class MiClase:
     def metodo_clase(cls):
         print(cls.variable_clase)
 
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
+
 print(MiClase.variable_clase)
 miClase1 = MiClase('esta es una variable de instancia')
 print(miClase1.variable_instancia)
@@ -32,3 +38,6 @@ print(miClase2.variable_clase2)
 
 MiClase.metodo_estatico()
 MiClase.metodo_clase()
+miObjeto1 = MiClase('variable de instancia')
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
