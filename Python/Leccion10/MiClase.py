c03debd097d0e98aeded65e38516c4d17741cca1
santy171004 <santinmartin773@gmail.com>
2025-10-14 12:14:57 -0300

    Clase 10 POO Parte 7 Diseño, Constante y contexto estático
    14.1 Creación de variables de clase

--- Python/Leccion10/MiClase.py
+++ Python/Leccion10/MiClase.py
@@ -13,3 +13,8 @@ miClase2 = MiClase('Esta es otra prueba de variable de instancia')
 print(miClase2.variable_instancia)
 print(miClase2.variable_clase)
 
+MiClase.variable_clase2 = 'Valor de variable clase 2'
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
