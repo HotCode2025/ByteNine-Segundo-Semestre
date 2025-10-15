a8b9f7c594448b1786ef6bb4f9cd51ac6ed63d1f
Nahuel Spikerman <nahuelspikerman@gmail.com>
2025-10-15 20:44:36 -0300

    Clase 10 - 10.1 Sobrecarga de constructores - Java

--- Java/Leccion6/Herencia/src/test/TestHerencia.java
+++ Java/Leccion6/Herencia/src/test/TestHerencia.java
@@ -3,6 +3,7 @@ package test;
 
 import domain.Cliente;
 import domain.Empleado;
+import domain.Persona;
 import java.util.Date;
 
 public class TestHerencia {
@@ -10,9 +11,11 @@ public class TestHerencia {
         Empleado empleado1 = new Empleado("Ariel", 57000.0);
         System.out.println("empleado1 = " + empleado1);
         
-        Date fecha1 = new Date();
-        
-        Cliente cliente1 = new Cliente(fecha1, true, "Valeria", 'F', 44, "San Jose, Tupungato, Mendoza");
-        System.out.println("cliente1 = " + cliente1);
+//        Date fecha1 = new Date();
+//        
+//        Cliente cliente1 = new Cliente(fecha1, true, "Valeria", 'F', 44, "San Jose, Tupungato, Mendoza");
+//        System.out.println("cliente1 = " + cliente1);
+//        
+//        Persona persona1 = new Persona();
     }
 }
