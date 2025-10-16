abb10fda0282a3967afd2a2d94e3e3d6ee2c74e7
Maximiliano <maxiarielmorales712@gmail.com>
2025-10-16 16:58:36 -0300

    10.3 Paquetes Parte 1

--- Java/Leccion7/SobrecargaMetodos/src/test/TestOperaciones.java
+++ Java/Leccion7/SobrecargaMetodos/src/test/TestOperaciones.java
@@ -7,12 +7,12 @@ import operaciones.Operaciones;
 public class TestOperaciones {
     public static void main(String[] args){
         var resultado = Operaciones.sumar(7, 9);
-        System.out.println("Resultado "+ resultado);
+        System.out.println("Resultado = " + resultado);
         
         var resultado2 = Operaciones.sumar(5.0, 7);
-        System.out.println("Resultado "+ resultado2);
+        System.out.println("Resultado = " + resultado2);
         
         var resultado3 = Operaciones.sumar(8, 6L);
-        System.out.println("Resultado "+ resultado3);
+        System.out.println("Resultado = " + resultado3);
     }
 }
