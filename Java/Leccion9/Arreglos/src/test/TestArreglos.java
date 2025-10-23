051a8a35e2286d3ea9e703814bc11a68f8d91afd
GabrielSantarena <gabrielsantarena69@gmail.com>
2025-10-23 18:31:48 -0300

    Clase 11 Arreglos o Arrays
    11.1 Arreglos Parte 1: Creamos la carpeta Leccion9

---
+++ Java/Leccion9/Arreglos/src/test/TestArreglos.java
@@ -0,0 +1,14 @@
+package test;
+
+
+public class TestArreglos {
+    //11.1 Arreglos Parte 1: Creamos la carpeta Leccion9
+
+    public static void main(String[] args) { //Lado derecho instanciamos un objeto de tipo object
+        
+        int edades[] = new int[3]; //El lado izquiero declaramos la variable
+        
+        System.out.println("edades = " + edades);
+        
+    }
+}
