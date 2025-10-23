bdec8379305d858b359ea7324287de5f3bccbc05
GabrielSantarena <gabrielsantarena69@gmail.com>
2025-10-23 18:11:45 -0300

    Clase 11 POO Parte 8 Diseño de clases y Sobrecarga de Operadores
    15.4 Sobrecarga de Operadores: Parte 1a hacerlo en carpeta Lección12

---
+++ Python/Leccion12/sobrecarga_operadores.py
@@ -0,0 +1,16 @@
+#15.4 Sobrecarga de Operadores: Parte 1a hacerlo en carpeta Lección12
+
+#Datos de tipo eentero
+a = 3
+b = 5
+print(a+b) #Suma
+
+#Datos de tipo string
+a = "Hola "
+b = "Mundo"
+print(a+b) #Concatena
+
+#Datos de tipo lista
+a = [3, 4, 5]
+b = [6, 7, 8 ,9]
+print(a+b) #Une
