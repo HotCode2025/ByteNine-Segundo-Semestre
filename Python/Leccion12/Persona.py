ce47444ca6a4064dffb0f47c0684cccbe8ae1dd9
GabrielSantarena <gabrielsantarena69@gmail.com>
2025-10-23 20:13:43 -0300

    Clase 11 POO Parte 8 Diseño de clases y Sobrecarga de Operadores
    15.7 Sobrecarga de Operadores: Parte 2b

--- Python/Leccion12/Persona.py
+++ Python/Leccion12/Persona.py
@@ -7,12 +7,13 @@ class Persona:
     def __add__(self, other): # Other significa = otro
         return f'{self.nombre} {other.nombre}'
 
+    def __sub__(self, otro):
+        return self.edad - otro.edad  #15.7 Sobrecarga de Operadores: Parte 2b
+
 persona1 = Persona('Ariel', 40)
 persona2 = Persona('Betancud', 5)
 
 # persona1.__add__(persona2) sintaxis interna y automática
 
 print(persona1 + persona2)
-
-
-
+print(persona1 - persona2) # 15.7 Sobrecarga de Operadores: Parte 2b
