6527753749043788db7ccf170923c67e5aff90ab
AgusSantarena <santarenaagustin69@gmail.com>
2025-10-23 20:37:11 -0300

    Clase 11 Arreglos o Arrays
    11.3 Arreglos Parte 3

--- Java/Leccion9/Arreglos/src/test/TestArreglos.java
+++ Java/Leccion9/Arreglos/src/test/TestArreglos.java
@@ -12,5 +12,17 @@ public class TestArreglos {
         
         edades[0] = 17;
         System.out.println("edades 0 = " + edades[0]);
+        
+        edades[1] = 22;
+        System.out.println("edades = " + edades[1]);
+        
+        edades[2] = 18;
+        System.out.println("edades = " + edades[2]);
+        
+        //edades[3] = 7;//Fuera de rango, error en tiempo de ejecucion
+        
+        for(int i = 0; i < edades.length;i++){
+            System.out.println("Edades y sus elementos "+(i+1)+": "+edades[i]);
+        }
     }
 }
