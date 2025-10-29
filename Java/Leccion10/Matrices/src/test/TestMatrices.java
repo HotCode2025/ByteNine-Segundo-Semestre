73dae4582c48b09ae6ec04d08d0fb24bae6bf010
Maximiliano <maxiarielmorales712@gmail.com>
2025-10-29 15:41:19 -0300

    Clase 11 Matrices Java: 12.3, 12.4, 12.5, Finalizada

--- Java/Leccion10/Matrices/src/test/TestMatrices.java
+++ Java/Leccion10/Matrices/src/test/TestMatrices.java
@@ -1,6 +1,8 @@
 
 package test;
 
+import domain.Persona;
+
 
 public class TestMatrices {
     public static void main(String[] args) {
@@ -10,8 +12,8 @@ public class TestMatrices {
         edades[0][1] = 7; //Es una diferente columna
         edades[1][0] = 8;
         edades[1][1] = 4;
-        edades[2][0] = 6;
-        edades[2][1] = 3;
+        edades[2][0] = 2;
+        edades[2][1] = 9;
         
         System.out.println("edades 0-0 = " + edades[0][0]);
         System.out.println("edades 0-1 = " + edades[0][1]);
@@ -19,6 +21,41 @@ public class TestMatrices {
         System.out.println("edades 1-1 = " + edades[1][1]);
         System.out.println("edades 2-0 = " + edades[2][0]);
         System.out.println("edades 2-1 = " + edades[2][1]);
+        System.out.println("Recorremos la matriz a través del ciclo for");
+        for (int fila = 0; fila < edades.length; fila++){
+            for(int col = 0; col < edades[fila].length; col++){
+                System.out.println("edades "+fila+"-"+col+": "+edades[fila][col]);
+            }
+        }
+        //Sintaxis clásica
+        //String frutas[][] = new String[3][2];
+        
+        //Sintacis simplificada
+        String frutas[][] = {{"Limon", "Pomelo"},{"Ciruela", "Kiwi"},{"Banana", "Manzana"}};
+        imprimir(frutas);
+//        for (int i = 0; i < frutas.length; i++){
+//            for(int j = 0; j < frutas[i].length; j++){
+//                System.out.println("frutas "+i+"-"+j+": "+frutas[i][j]);
+//            }
+//        } 
+       //Creamos una matriz de objetos
+       Persona personas[][] = new Persona[2][3];
+       //Signamos valores a la matriz
+       personas[0][0] = new Persona("Ariel");
+       personas[0][1] = new Persona("Osvaldo");
+       personas[0][2] = new Persona("Maxi");
+       personas[1][0] = new Persona("Morales");
+       personas[1][1] = new Persona("Valeria");
+       personas[1][2] = new Persona("Lardin");
+        System.out.println("Matriz de Personas: ");
+       imprimir(personas);
     }
     
+    public static void imprimir(Object matriz[][]){
+        for (int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                System.out.println("matriz "+i+"-"+j+": "+matriz[i][j]);
+            }
+        }
+    }
 }
