4bdc35d32f6a39ad0fe24dca4d959293d0771719
AgusSantarena <santarenaagustin69@gmail.com>
2025-10-29 22:29:39 -0300

    Clase 12 Diseño de clases
    13.6 Comenzamos con la clase Orden: Parte 4

--- Java/Leccion11/Ventas/src/ar/com/codesystem/ventas/Orden.java
+++ Java/Leccion11/Ventas/src/ar/com/codesystem/ventas/Orden.java
@@ -33,4 +33,16 @@ public class Orden {
         }
         return total;
     }
+    
+    public void mostrarOrden() {
+        System.out.println("Id Orden: "+idOrden);
+        double totalOrden = this.calcularTotal();
+        System.out.println("El total de la orden es: $"+totalOrden);
+        System.out.println("Productos de la orden: ");
+    
+        for(int i = 0; i < this.contadorProductos; i++){
+            System.out.println(this.productos[i]);
+        }
+    }
+
 }
