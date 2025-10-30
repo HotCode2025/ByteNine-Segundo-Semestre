538bbe788db7c56460d712294f87f6f22a637500
GabrielSantarena <gabrielsantarena69@gmail.com>
2025-10-29 21:45:12 -0300

    Clase 12 Diseño de clases
    13.4 Comenzamos con la clase Orden: Parte 2

--- Java/Leccion11/Ventas/src/ar/com/codesystem/ventas/Orden.java
+++ Java/Leccion11/Ventas/src/ar/com/codesystem/ventas/Orden.java
@@ -13,4 +13,13 @@ public class Orden {
         this.idOrden = ++Orden.contadorOrdenes;
         this.productos = new Producto[Orden.MAX_PRODUCTOS];
     }
+    
+    // 13.4 Comenzamos con la clase Orden: Parte 2
+    public void agregarProducto(Producto producto){
+        if(this.contadorProductos < Orden.MAX_PRODUCTOS){
+            this.productos[this.contadorProductos++] = producto;
+        }else{
+            System.out.println("Se ah superado el maximo de productos: "+Orden.MAX_PRODUCTOS);
+        }
+    }
 }
