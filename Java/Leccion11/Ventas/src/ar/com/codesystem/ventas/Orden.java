162acf407d13dae6bcade6f53158e64dd7ad1918
Maximiliano <maxiarielmorales712@gmail.com>
2025-10-29 22:20:18 -0300

    13.5 Comenzamos con la clase Orden: Parte 3

--- Java/Leccion11/Ventas/src/ar/com/codesystem/ventas/Orden.java
+++ Java/Leccion11/Ventas/src/ar/com/codesystem/ventas/Orden.java
@@ -24,4 +24,13 @@ public class Orden {
         }
     }
     
+    public double calcularTotal(){
+        double total = 0; //Variable temporal
+        for (int i = 0; i < this.contadorProductos; i++) {
+//            Producto producto = this.productos[i];
+//            total += producto.getPrecio();
+            total += this.productos[i].getPrecio();
+        }
+        return total;
+    }
 }
